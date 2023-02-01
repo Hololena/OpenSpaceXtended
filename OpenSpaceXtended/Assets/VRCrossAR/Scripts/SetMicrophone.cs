43008216b9e378cf4b4a49e30c620d2d114c19ff
Hololena <lena.biresch@gmx.de>
2023-02-01 17:29:44 +0100

    Project

---
+++ OpenSpaceXtended/Assets/VRCrossAR/Scripts/SetMicrophone.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Pun;
+using UnityEngine;
+//using Photon.Voice.Unity;
+
+public class SetMicrophone : MonoBehaviourPun
+{
+    //detects device microphone and sets it to "Recorder" component from Photon Voice
+    private void Start()
+    {
+        //string[] devices = Microphone.devices;
+        //if (devices.Length > 0)
+        //{
+        //    Recorder recorder = GetComponent<Recorder>();
+        //    GetComponent<Recorder>().MicrophoneDevice = new DeviceInfo(devices[0]);
+        //    recorder.UnityMicrophoneDevice = devices[0];
+        //}
+    }
+}
